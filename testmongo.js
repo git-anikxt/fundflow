4f25188281fbd1ea7733636ce05cf1ae59539318
Aniket Sharma <anikxt018@gmail.com>
2026-06-06 11:44:16 +0530

    Delete testmongo.js

--- testmongo.js
+++
@@ -1,9 +0,0 @@
-require("dotenv").config({ path: ".env.local" });
-
-console.log("MONGO_URI =", process.env.MONGO_URI);
-
-const mongoose = require("mongoose");
-
-mongoose.connect(process.env.MONGO_URI)
-.then(() => console.log("CONNECTED"))
-.catch(err => console.error(err));
